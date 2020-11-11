4055709bc810d0b32b091dc309a8885fbe6e882a
Logan Morro <loganfmorro@gmail.com>
2020-11-11 15:32:24 -0500

    added code to define and export manager information

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,13 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+    }
+    getEmployeeRole() { return "Manager"; }
+    getManagerOffice() { return this.officeNum; }
+}
+
+module.exports = Manager;
